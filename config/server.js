5a291d9e4b93c24d883296d543029d8479acf2ce
iwegbu victor <iwegbuvc@gmail.com>
2025-02-20 04:13:31 +0100

    updated server port

--- config/server.js
+++ config/server.js
@@ -9,16 +9,31 @@
 //   },
 // });
 
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', process.env.PORT || 10000), // Use Render's assigned port
+//   app: {
+//     keys: env.array('APP_KEYS', [
+//       'defaultKey1',
+//       'defaultKey2',
+//       'defaultKey3',
+//       'defaultKey4'
+//     ]), // Ensure fallback keys
+//   },
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+// });
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', process.env.PORT || 10000), // Use Render's assigned port
+  port: env.int('PORT', process.env.PORT), // Use Render's assigned port
   app: {
     keys: env.array('APP_KEYS', [
       'defaultKey1',
       'defaultKey2',
       'defaultKey3',
       'defaultKey4'
-    ]), // Ensure fallback keys
+    ]),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
